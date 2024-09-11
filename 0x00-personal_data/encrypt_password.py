41b712b467347770d42f9e370ab90550a98677a0
Martin Kihungi <mwangim4786@gmail.com>
2024-09-11 23:34:54 +0000

    encrypt_password.py 1

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -2,7 +2,6 @@
 """ encrypting passwords """
 import bcrypt
 
-
 def hash_password(password: str) -> bytes:
     """ expects one string argument name password and returns
         a salted, hashed password, which is a byte string. 
