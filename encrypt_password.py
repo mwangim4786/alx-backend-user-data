72f930ecaf515934c8514061507363a6879edeea
Martin Kihungi <mwangim4786@gmail.com>
2024-09-12 02:24:03 +0300

    ALL

---
+++ encrypt_password.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" encrypting passwords """
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """ expects one string argument name password and returns
+        a salted, hashed password, which is a byte string. 
+    """
+    if password:
+        return bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """  expects 2 arguments and returns a boolean.
+        Use bcrypt to validate that the provided password
+        matches the hashed password.
+    """
+    if hashed_password and password:
+        return bcrypt.checkpw(str.encode(password), hashed_password)
