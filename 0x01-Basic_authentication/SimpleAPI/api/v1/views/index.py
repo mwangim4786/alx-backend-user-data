b28bbf2efa38e256f5778bfddd82e579c6d9552a
Martin Kihungi <mwangim4786@gmail.com>
2024-09-16 17:11:43 +0300

    api/v1/app.py, api/v1/views/index.py

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - the status code for request unauthorized.
+    """
+    return abort(401, "Unauthorized")
