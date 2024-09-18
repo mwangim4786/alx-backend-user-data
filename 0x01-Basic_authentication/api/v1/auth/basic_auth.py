d804186072fd3dcb2b0812bee829746a69976e7a
Martin Kihungi <mwangim4786@gmail.com>
2024-09-18 17:07:10 +0300

    basic_auth.py

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+    basic_auth module for the API
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """sumary_line
+    
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
+    pass
