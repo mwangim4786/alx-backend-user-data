68ea4735e8e5d1814e4731fd33e9317cda7e2f42
Martin Kihungi <mwangim4786@gmail.com>
2024-09-19 01:46:02 +0300

    basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -12,4 +12,28 @@ class BasicAuth(Auth):
     argument -- description
     Return: return_description
     """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """sumary_line
+        
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        value = authorization_header.split(' ')[1]
+        return value
+
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """sumary_line
+        
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        pass
