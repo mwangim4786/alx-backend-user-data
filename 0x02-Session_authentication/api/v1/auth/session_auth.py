dc19d9aadfa8926381ba16f3f6080dd216a8f0a0
Martin Kihungi <mwangim4786@gmail.com>
2024-09-19 17:04:56 +0300

    task 2 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,11 @@
 """
     session_auth module for the API
 """
+from base64 import b64decode
 import uuid
 from api.v1.auth.auth import Auth
+from typing import List, TypeVar
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -17,8 +20,10 @@ class SessionAuth(Auth):
         """
         if user_id is None:
             return None
+        
         if not isinstance(user_id, str):
             return None
+        
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
