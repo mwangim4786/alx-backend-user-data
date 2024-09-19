bdab82c3089664b82e678c1be9290ba58a707b98
Martin Kihungi <mwangim4786@gmail.com>
2024-09-20 02:30:34 +0300

    task 5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -31,15 +31,16 @@ def before_request() -> Optional[str]:
     """
     allowed_paths = ['/api/v1/status/',
                      '/api/v1/unauthorized/',
-                     '/api/v1/forbidden/']
+                     '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if auth is None:
         return
 
     if not auth.require_auth(request.path, allowed_paths):
         return
 
-    if auth.authorization_header(request) is None:
-        return abort(401)
+    if not auth.authorization_header(request):
+        if not auth.session_cookie(request):
+            return abort(401)
 
     if auth.current_user(request) is None:
         return abort(403)
