65161edbc725b2ad5e1e951bdde221a0ffac593f
Martin Kihungi <mwangim4786@gmail.com>
2024-09-19 17:04:04 +0300

    task 2 1

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,11 +2,8 @@
 """
     session_auth module for the API
 """
-from base64 import b64decode
 import uuid
 from api.v1.auth.auth import Auth
-from typing import List, TypeVar
-from models.user import User
 
 
 class SessionAuth(Auth):
