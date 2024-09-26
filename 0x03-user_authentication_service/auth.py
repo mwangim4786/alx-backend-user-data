323c47b0bbdaf0f286d69b0edc23574a7f870518
Martin Kihungi <mwangim4786@gmail.com>
2024-09-27 01:33:06 +0300

    auth.py initial flask

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """ Creates user Database table """
 import bcrypt
+from sqlalchemy.orm.exc import NoResultFound
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -9,3 +12,27 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_pass = bcrypt.hashpw(encoded_pass, salt)
     return hashed_pass
+
+
+class Auth:
+    """
+
+    """
+    def __init__(self) -> None:
+        """  """
+        self.__db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """  """
+        try:
+            existing_user = self.__db.find_user_by(email=email)
+            if existing_user:
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            pass
+
+        hash_pass = _hash_password(password)
+        new_user = self.db.add_user(email=email,
+                                    hashed_password=hash_pass.decode("utf-8"))
+        self.__db
+        return new_user
