8ae27a44a2a16ba8ec1cc562a67d807e628540e7
Martin Kihungi <mwangim4786@gmail.com>
2024-09-26 22:57:03 +0300

    user.py 5

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,12 +3,11 @@
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 
-
 Base = declarative_base()
 
 
 class User(Base):
-    """Representation of table user """
+    """ Representation of table user """
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
