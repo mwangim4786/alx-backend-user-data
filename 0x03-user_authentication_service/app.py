e591b1ea2b842b96aa3997599d39a7764d054f7e
Martin Kihungi <mwangim4786@gmail.com>
2024-09-27 01:35:18 +0300

    app.py initial flask

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module to run flask app
+"""
+from flask import Flask, jsonify, Response
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home() -> Response:
+    """
+    Home route
+    """
+    message = {"message": "Bienvenue"}
+    return jsonify(message)
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
