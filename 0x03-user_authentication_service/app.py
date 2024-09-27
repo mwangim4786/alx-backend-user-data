73e24e9fd71b1475924c2b882898d2e33165eb7f
Martin Kihungi <mwangim4786@gmail.com>
2024-09-27 03:36:27 +0300

    app.py auth.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 Module to run flask app
 """
-from flask import Flask, jsonify, Response, request, abort
+from flask import Flask, jsonify, Response, request, abort, redirect
 from auth import Auth
 
 AUTH = Auth()
@@ -50,6 +50,20 @@ def login() -> str:
     response = jsonify({"email": f"{email}", "message": "logged in"})
     response.set_cookie('session_id', session_id)
     return response
+
+
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+def logout() -> str:
+    """DELETE /sessions
+    Return:
+        - Redirects to home route.
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect("/")
 
 
 if __name__ == "__main__":
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -95,3 +95,15 @@ class Auth:
             return user.email
         except NoResultFound:
             return
+
+    def destroy_session(self, user_id: int) -> None:
+        """destroy session
+
+        Args:
+            user_id (int): user id
+        """
+        try:
+            user = self._db.find_user_by(id=user_id)
+            self._db.update_user(user.id, session_id=None)
+        except NoResultFound:
+            pass
