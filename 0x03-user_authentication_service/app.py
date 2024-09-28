633ae540907e83d56f3f24dfc5af49983c04413f
Martin Kihungi <mwangim4786@gmail.com>
2024-09-28 04:37:57 +0300

    print

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -80,7 +80,7 @@ def profile() -> str:
     """
     session_id = request.cookies.get("session_id", None)
     existing_user = AUTH.get_user_from_session_id(session_id)
-    return existing_user
+    print(existing_user)
 
 
 @app.route('/reset_password', methods=['POST'], strict_slashes=False)
